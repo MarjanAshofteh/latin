bbe74fd0dc76908bd2ab4789923d67875e857776
MarjanAshofteh <marjan.cs90@gmail.com>
2018-08-28 12:42:44 +0430

    add audio player and some other small changes

--- src/components/node.vue
+++ src/components/node.vue
@@ -25,6 +25,9 @@
       />
     </div>
     <md-content class="node_body">
+      <div v-if="types.includes('podcast')" id="audio-demos-vuejs">
+        <wavesurferPlayer layout="modal"></wavesurferPlayer>
+      </div>
       <embedVideo v-if="types.includes('video') && node_content.hasOwnProperty('video_link') && (node_content.video_link != null)"
         :url="node_content.video_link"/>
       <img v-if="node_content.hasOwnProperty('image') && (node_content.image != null)" style="margin: 25px 0 0 0; max-width: 900px;" :src="node_content.image | createlink" :alt="node_content.title">
@@ -55,6 +58,7 @@ import eventData from '@/components/eventData'
 import author from '@/components/author'
 import tag from '@/components/tag'
 import embedVideo from '@/components/embedVideo'
+import wavesurferPlayer from '@/components/wavesurferPlayer'
 import axios from 'axios'
 export default {
   name: 'node',
@@ -73,7 +77,8 @@ export default {
     eventData,
     author,
     tag,
-    embedVideo
+    embedVideo,
+    wavesurferPlayer
   },
   mounted(){
     axios.get('http://api.ed808.com/latin/latin_contents/'+ this.nid + '?parameter[hash]=f275ebb87f408796b11f651b929293edf639554efb9e014c53c8b8d8e0f9db45',
@@ -244,5 +249,9 @@ export default {
     .node_body > img{
       display: none;
     }
+  }
+  #audio-demos-vuejs{
+    margin: 30px auto;
+    padding: 0 30px;
   }
 </style>
