04058879275af71e6cf7027798a6c8c301847c7c
meysam razmi <m.razmi.92@gmail.com>
2018-09-30 18:45:09 +0330

    add tags page

--- src/components/tags/tags.vue
+++ src/components/tags/tags.vue
@@ -1,11 +1,28 @@
 <template>
-  <div style="padding-top: 15px;">
-      {{tid}},{{tname}}
-    <div class="md-layout" style="align-items: flex-start; padding: 0 0 30px 15px;">
-        <filters class="md-layout-item md-size-20" style="padding: 0px 15px 0 0;"/>
-        <NodeList :filterEnabled="true" class="md-card md-theme-default md-layout-item md-size-80"/>
+  <div class="md-layout md-content" style="text-align: left;">
+    <div class="loading" v-if="loading">
+      <md-progress-bar md-mode="indeterminate" md-theme-default></md-progress-bar>
     </div>
+    
+    <md-card class="md-layout-item md-size-95 pad-15" style="margin: 15px auto;"><md-card-content>
+        <div class="top_header md-layout-item md-size-100 pad-15">
+            <h1>{{name}}</h1>
+        </div>
+        <article class="md-layout-item md-size-100 pad-15"
+            v-html="description"
+        ></article></md-card-content>
+    </md-card>
 
+    <div class="md-layout-item md-size-100 pad-15 md-layout">
+        <div class="md-headline md-layout-item md-size-100" style="padding: 35px 25px 15px 25px;">
+            <h5>Articles</h5>
+        </div>
+        <NodeTeaser v-for="content in articles" :key="content.nid"
+            :title="content.title" 
+            :pic="content.picture"
+            :nid="content.nid"
+            :date="content.created"/>
+    </div>
   </div>
 </template>
 
@@ -13,26 +30,67 @@
 import axios from 'axios'
 import NodeList from '@/components/allContents/NodeList'
 import filters from '@/components/allContents/filters'
+import NodeTeaser from '@/components/allContents/NodeTeaser'
 
 export default {
     name: 'tags',
     data(){
       return {
         tid: this.$route.params.tid,
-        tname: this.$route.params.tname,
+        name: this.$route.params.tname,
         loading:true,
+        description: '',
         errors:'',
-        showError:false
+        showError:false,
+        articles: [],
       }
     },
-  components: {
-    NodeList,
-    filters
-  },
+    mounted(){
+        axios.get('http://api.ed808.com/latin/tag/'+ this.tid + '/contents')
+        .then((data) => data.data)
+        .then((data) => {
+            console.log(data)
+            this.name = data.tag.name
+            this.description = data.tag.description
+            this.articles = data.contents.article
+
+            this.loading = false
+        })
+    },
+    components: {
+        NodeList,
+        filters,
+        NodeTeaser
+    },
 }
 </script>
 
 <style lang="scss">
-
+.pad-15{
+    padding: 15px;
+}
+.md-headline{
+	padding: 35px 0;
+	margin: 0 !important;
+	position: relative;
+  text-align: left;
+  &:before {
+    content: "";
+    width: 100%;
+    height: 1px;
+    background: #ddd;
+    position: absolute;
+    top: 53px;
+  }
+  h5{
+    margin: 0;
+    font-weight: normal;
+    display: inline-block;
+    background: #fafafa;
+    z-index: 1;
+    position: relative;
+    padding-right: 15px;
+  }
+}
 </style>
 
