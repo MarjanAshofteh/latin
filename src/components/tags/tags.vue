843470d49d4fc6f3861ed80128b84b66fe842fe5
meysam razmi <m.razmi.92@gmail.com>
2018-10-02 18:58:52 +0330

    change tags pages and did some styling

--- src/components/tags/tags.vue
+++ src/components/tags/tags.vue
@@ -1,66 +1,58 @@
 <template>
-  <div class="md-layout md-content" style="text-align: left;">
-    <div class="loading" v-if="loading">
+  <div class="md-layout container">
+
+    <div class="loading" v-if="loading" style="width: 100%;">
       <md-progress-bar md-mode="indeterminate" md-theme-default></md-progress-bar>
     </div>
     
-    <md-card class="md-layout-item md-size-95 pad-15" style="margin: 15px auto;"><md-card-content>
-        <div class="top_header md-layout-item md-size-100 pad-15">
-            <h1>{{name}}</h1>
-        </div>
-        <article class="md-layout-item md-size-100 pad-15"
-            v-html="description"
-        ></article></md-card-content>
+    <md-card class="title md-layout-item md-size-100 pad-15">
+        <breadcrumb :links="[{url:'/tags/'+ tid +'/'+ name, title: 'tag: '+ name}]"></breadcrumb>
+        <md-card-content>
+            <div class="top_header md-layout-item md-size-100">
+                <h1>{{name}}</h1>
+            </div>
+            <article class="md-layout-item md-size-100"
+                v-html="description"
+            ></article>
+        </md-card-content>
     </md-card>
 
-    <div class="md-layout-item md-size-100 pad-15 md-layout">
-        <div class="md-headline md-layout-item md-size-100" style="padding: 35px 25px 15px 25px;">
-            <h5>Articles</h5>
-        </div>
-        <NodeTeaser v-for="content in articles" :key="content.nid"
-            :title="content.title" 
-            :pic="content.picture"
-            :nid="content.nid"
-            :date="content.created"/>
+    <div class="md-size-100 md-layout-item">
+        <NodeList :filterEnabled="false" :router="'allContents'" :tag="tid" class="md-card md-theme-default md-layout-item md-size-80"/>
     </div>
+
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import NodeList from '@/components/allContents/NodeList'
-import filters from '@/components/allContents/filters'
-import NodeTeaser from '@/components/allContents/NodeTeaser'
+import breadcrumb from '@/components/fields/breadcrumb'
 
 export default {
     name: 'tags',
+    components: {
+        NodeList,
+        breadcrumb
+    },
     data(){
       return {
         tid: this.$route.params.tid,
         name: this.$route.params.tname,
-        loading:true,
         description: '',
+        loading:true,
         errors:'',
         showError:false,
-        articles: [],
       }
     },
     mounted(){
         axios.get('http://api.ed808.com/latin/tag/'+ this.tid + '/contents')
         .then((data) => data.data)
         .then((data) => {
-            console.log(data)
-            this.name = data.tag.name
-            this.description = data.tag.description
-            this.articles = data.contents.article
-
+            this.name = data.name
+            this.description = data.description
             this.loading = false
         })
-    },
-    components: {
-        NodeList,
-        filters,
-        NodeTeaser
     },
 }
 </script>
@@ -68,6 +60,18 @@ export default {
 <style lang="scss">
 .pad-15{
     padding: 15px;
+}
+.container {
+    text-align: left;
+    > .md-size-100{
+        margin: 15px auto !important;
+        max-width: calc(100% - 30px);
+        min-width: inherit;
+    }
+}
+.title .md-card-content{
+    border: solid #eee;
+    border-width: 1px 0;
 }
 .md-headline{
 	padding: 35px 0;
