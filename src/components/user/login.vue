2fcf16a8274f38665c9d3fb187ec68437caae6b2
meysam razmi <m.razmi.92@gmail.com>
2018-10-07 18:35:08 +0330

    add recaptcha for login and register pages

--- src/components/user/login.vue
+++ src/components/user/login.vue
@@ -1,34 +1,40 @@
 <template>
     <div>
-        <form novalidate class="md-layout" @submit.prevent="validateUser">
-        <md-card class="md-layout-item md-size-30 md-small-size-100">
+        <form novalidate class="md-layout" @submit.prevent="formSubmit">
+        <md-card class="md-layout-item md-size-30 md-small-size-100" style="overflow: visible;">
             <md-card-header>
             <div class="md-title" style="margin: 0;color: white;font-size: 25px;">Login</div>
             </md-card-header>
 
             <md-card-content>
-    
-            <div class="md-layout-item md-small-size-100 md-size-100">
-                <md-field :class="getValidationClass('emailOrUsename')">
-                <label for="email">Email or Username</label>
-                <md-input name="username_email" id="username_email" autocomplete="email" v-model="form.username_email" :disabled="sending" required/>
-                <span class="md-error" v-if="!$v.form.username_email.required">The email or usename is required</span>
-                </md-field>
-            </div>
-            <div class="md-layout-item md-small-size-100 md-size-100">
-                <md-field :class="getValidationClass('password')">
-                <label for="password">Password</label>
-                <md-input type="password" id="password" name="password" v-model="form.password" :disabled="sending" autocomplete="current-password" required/>
-                <span class="md-error" v-if="!$v.form.password.required">The Password is required</span>
-                </md-field>
-            </div>
-
+              <div class="md-layout-item md-small-size-100 md-size-100">
+                  <md-field :class="getValidationClass('emailOrUsename')">
+                  <label for="email">Email or Username</label>
+                  <md-input name="username_email" id="username_email" autocomplete="email" v-model="form.username_email" :disabled="sending" required/>
+                  <span class="md-error" v-if="!$v.form.username_email.required">The email or usename is required</span>
+                  </md-field>
+              </div>
+              <div class="md-layout-item md-small-size-100 md-size-100">
+                  <md-field :class="getValidationClass('password')">
+                  <label for="password">Password</label>
+                  <md-input type="password" id="password" name="password" v-model="form.password" :disabled="sending" autocomplete="current-password" required/>
+                  <span class="md-error" v-if="!$v.form.password.required">The Password is required</span>
+                  </md-field>
+              </div>
             </md-card-content>
 
             <md-progress-bar md-mode="indeterminate" v-if="sending" />
+            
+            <vue-recaptcha
+              ref="recaptcha"
+              @verify="logUserIn"
+              @expired="onCaptchaExpired"
+              size="invisible"
+              sitekey="6Ldp3XMUAAAAACZav47_l9to_2uESNGLa1RvQOU6">
+            </vue-recaptcha>
 
             <md-card-actions>
-            <md-button type="submit" class="md-primary" :disabled="sending">Log In</md-button>
+              <md-button type="submit" class="md-primary" :disabled="sending">Log In</md-button>
             </md-card-actions>
         </md-card>
         <md-snackbar :md-active.sync="userSaved">{{ lastUser }} you are log in successfully!</md-snackbar>
@@ -38,6 +44,7 @@
 </template>
 <script>
 import axios from 'axios'
+import VueRecaptcha from 'vue-recaptcha'
 import { validationMixin } from 'vuelidate'
 import { required } from 'vuelidate/lib/validators'
 import { cookie } from '@/components/mixins/cookie.js'
@@ -45,6 +52,9 @@ import { cookie } from '@/components/mixins/cookie.js'
 export default {
   name: 'login',
   mixins: [validationMixin,cookie],
+  components: {
+    'vue-recaptcha': VueRecaptcha
+  },
   data(){
     return{
       errors:'',
@@ -85,16 +95,23 @@ export default {
         this.form.username_email = null
         this.form.password = null
     },
-    logUserIn(){
+    logUserIn(recaptchaToken){
       this.sending = true
-      var data = this.createPostData
+      this.$refs.recaptcha.reset();
+
+      //sending to login api
       axios.defaults.crossDomain = true;
       axios.defaults.withCredentials  = true;
       axios.post('http://api.ed808.com/latin/user/login',
-      data,
+      {
+        hash : "50e185c2e0c2bc30215338db776022c92ecbc441fd933688c6bf4f274c863c60",
+        username_email : this.form.username_email,
+        password : this.form.password,
+        reCaptchaToken : recaptchaToken
+      },
       {
         headers: {
-        'Content-type': 'application/json'
+          'Content-type': 'application/json'
         }
       })
       .then((data) => {
@@ -130,11 +147,16 @@ export default {
         /*todo : showing error without [""] */
       });
     },
-    validateUser(){
+    formSubmit(){
+      //validating form
       this.$v.$touch()
-      if (!this.$v.$invalid) {
-        this.logUserIn()
+      //run recaptcha and let it to fire up 'logUserIn' function with google token
+      if (!this.$v.$invalid){
+        this.$refs.recaptcha.execute();
       }
+    },
+    onCaptchaExpired: function () {
+      this.$refs.recaptcha.reset();
     }
   },
   mounted(){
@@ -164,15 +186,6 @@ export default {
       })
     }
   },
-  computed:{
-    createPostData(){
-      return{
-        hash : "50e185c2e0c2bc30215338db776022c92ecbc441fd933688c6bf4f274c863c60",
-        username_email : this.form.username_email,
-        password : this.form.password
-      }
-    }
-  }
 }
 </script>
 
--- src/components/user/register.vue
+++ src/components/user/register.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <form novalidate class="md-layout" @submit.prevent="validateUser">
-      <md-card class="md-layout-item md-size-30 md-small-size-100">
+    <form novalidate class="md-layout" @submit.prevent="formSubmit">
+      <md-card class="md-layout-item md-size-30 md-small-size-100" style="overflow: visible;">
         <md-card-header>
           <div class="md-title" style="margin: 0;color: white;font-size: 25px;">Register</div>
         </md-card-header>
@@ -56,6 +56,14 @@
 
         <md-progress-bar md-mode="indeterminate" v-if="sending" />
 
+        <vue-recaptcha
+          ref="recaptcha"
+          @verify="saveUser"
+          @expired="onCaptchaExpired"
+          size="invisible"
+          sitekey="6Ldp3XMUAAAAACZav47_l9to_2uESNGLa1RvQOU6">
+        </vue-recaptcha>
+
         <md-card-actions>
           <md-button type="submit" class="md-primary" :disabled="sending">Create user</md-button>
         </md-card-actions>
@@ -70,162 +78,172 @@
 </template>
 
 <script>
-  import axios from 'axios'
-  import { validationMixin } from 'vuelidate'
-  import {
-    required,
-    email,
-    minLength,
-    maxLength,
-    //numeric,
-    sameAs
-  } from 'vuelidate/lib/validators'
-  import { cookie } from '@/components/mixins/cookie.js'
+import axios from 'axios'
+import { validationMixin } from 'vuelidate'
+import {
+  required,
+  email,
+  minLength,
+  maxLength,
+  //numeric,
+  sameAs
+} from 'vuelidate/lib/validators'
+import VueRecaptcha from 'vue-recaptcha'
+import { cookie } from '@/components/mixins/cookie.js'
 
-  export default {
-    name: 'register',
-    mixins: [validationMixin,cookie],
-    data: () => ({
-      errors:'',
-      showError:false,
-      form: {
-        fullName: null,
-        email: null,
-        username: null,
-        password: null,
-        rePassword: null,
+export default {
+  name: 'register',
+  mixins: [validationMixin,cookie],
+  components: {
+    'vue-recaptcha': VueRecaptcha
+  },
+  data: () => ({
+    errors:'',
+    showError:false,
+    form: {
+      fullName: null,
+      email: null,
+      username: null,
+      password: null,
+      rePassword: null,
+    },
+    userSaved: false,
+    sending: false,
+    lastUser: null
+  }),
+  validations: {
+    form: {
+      fullName: {
+        required,
+        minLength: minLength(3)
       },
-      userSaved: false,
-      sending: false,
-      lastUser: null
-    }),
-    validations: {
-      form: {
-        fullName: {
-          required,
-          minLength: minLength(3)
-        },
-        email: {
-          required,
-          email
-        },
-        username: {
-          required,
-          //taken
-        },
-        password: {
-          required,
-          minLength: minLength(6)
-        },
-        rePassword: {
-          required,
-          sameAsPassword: sameAs('password')
+      email: {
+        required,
+        email
+      },
+      username: {
+        required,
+        //taken
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      },
+      rePassword: {
+        required,
+        sameAsPassword: sameAs('password')
+      }
+    }
+  },
+  methods: {
+    getValidationClass (fieldName) {
+      const field = this.$v.form[fieldName]
+
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
         }
       }
     },
-    methods: {
-      getValidationClass (fieldName) {
-        const field = this.$v.form[fieldName]
-
-        if (field) {
-          return {
-            'md-invalid': field.$invalid && field.$dirty
-          }
-        }
-      },
-      clearForm () {
-        this.$v.$reset()
-        this.form.fullName = null
-        this.form.email = null
-        this.form.username = null
-        this.form.password = null
-        this.form.rePassword = null
-        this.showError = false
-        this.errors = ''
-      },
-      saveUser () {
-        this.sending = true
-        var userData = {
-          "hash" : "50e185c2e0c2bc30215338db776022c92ecbc441fd933688c6bf4f274c863c60",
-          "username" : this.form.username,
-          "password" : this.form.password,
-          "email" : this.form.email,
-          "full_name" : this.form.fullName
-        }
-        axios.post('http://api.ed808.com/latin/user/register',
-          userData,
-          {
-            headers: {
-            'Content-type': 'application/json',
-            }
-          })
-          .then((data) => {
-            this.loguserin(data)
-            this.lastUser = `${this.form.fullName}`
-            this.userSaved = true
-            this.sending = false
-            this.$emit('do_navbar')
-            if(data.data.uid != 0){
-              this.setUid(data.data.uid)
-              this.$router.push('/user/'+ data.data.uid)
-            }
-            this.clearForm()
-          })
-          .catch(e => {
-            if(e.hasOwnProperty('response')){
-              if(e.response.hasOwnProperty('data'))
-                this.errors = e.response.data
-              else{
-                this.errors = e.response
-              }
-            }
-            else{
-              this.errors = e
-            }
-            this.showError = true
-          });
-      },
-      validateUser () {
-        this.$v.$touch()
-
-        if (!this.$v.$invalid) {
-          this.saveUser()
-        }
-      },
-      loguserin(data){
-        this.setUid(data.data.uid)
-        //save data to cookie storage
-        this.setCookie("token", data.data.token , 23)
-      }
+    clearForm () {
+      this.$v.$reset()
+      this.form.fullName = null
+      this.form.email = null
+      this.form.username = null
+      this.form.password = null
+      this.form.rePassword = null
+      this.showError = false
+      this.errors = ''
     },
-    mounted(){
-      if(this.$store.getters.getUid){
-        this.$router.push('/user/'+ this.$store.getters.getUid)
-      }
-      else{
-        //this is just work for admins that login in api.edu befor(has session and doesnt have token)
-        //this code is gonna set token for them
-        axios.defaults.crossDomain = true;
-        axios.defaults.withCredentials  = true;
-        axios.get('http://api.ed808.com/latin/user/login/nav_bar_info',
+    saveUser(recaptchaToken) {
+      this.sending = true
+      this.$refs.recaptcha.reset();
+
+      axios.post('http://api.ed808.com/latin/user/register',
         {
-          headers:{
-            'Content-type': 'application/json'
+          hash : "50e185c2e0c2bc30215338db776022c92ecbc441fd933688c6bf4f274c863c60",
+          username : this.form.username,
+          password : this.form.password,
+          email : this.form.email,
+          full_name : this.form.fullName,
+          reCaptchaToken : recaptchaToken
+        },
+        {
+          headers: {
+            'Content-type': 'application/json',
           }
         })
         .then((data) => {
+          this.loguserin(data)
+          this.lastUser = `${this.form.fullName}`
+          this.userSaved = true
+          this.sending = false
+          this.$emit('do_navbar')
           if(data.data.uid != 0){
-            this.setCookie("token", data.data.token , 23)
-            //under line is not working, I think.
-            this.$emit('do_navbar')
+            this.setUid(data.data.uid)
+            this.$router.push('/user/'+ data.data.uid)
           }
+          this.clearForm()
         })
         .catch(e => {
-          console.log('errors for nav_bar_info : ' + e)
-        })
+          if(e.hasOwnProperty('response')){
+            if(e.response.hasOwnProperty('data'))
+              this.errors = e.response.data
+            else{
+              this.errors = e.response
+            }
+          }
+          else{
+            this.errors = e
+          }
+          this.showError = true
+        });
+    },
+    formSubmit() {
+      //validating form
+      this.$v.$touch()
+      //run recaptcha and let it to fire up 'logUserIn' function with google token
+      if (!this.$v.$invalid) {
+        this.$refs.recaptcha.execute();
       }
+    },
+    onCaptchaExpired: function () {
+      this.$refs.recaptcha.reset();
+    },
+    loguserin(data){
+      this.setUid(data.data.uid)
+      //save data to cookie storage
+      this.setCookie("token", data.data.token , 23)
+    }
+  },
+  mounted(){
+    if(this.$store.getters.getUid){
+      this.$router.push('/user/'+ this.$store.getters.getUid)
+    }
+    else{
+      //this is just work for admins that login in api.edu befor(has session and doesnt have token)
+      //this code is gonna set token for them
+      axios.defaults.crossDomain = true;
+      axios.defaults.withCredentials  = true;
+      axios.get('http://api.ed808.com/latin/user/login/nav_bar_info',
+      {
+        headers:{
+          'Content-type': 'application/json'
+        }
+      })
+      .then((data) => {
+        if(data.data.uid != 0){
+          this.setCookie("token", data.data.token , 23)
+          //under line is not working, I think.
+          this.$emit('do_navbar')
+        }
+      })
+      .catch(e => {
+        console.log('errors for nav_bar_info : ' + e)
+      })
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
